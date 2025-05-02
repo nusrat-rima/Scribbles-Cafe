c588bd710ecc6d566f36897dcfb04d3b30bb89f7
nusrat-rima <nusratrima3151@gmail.com>
2025-05-02 21:24:08 +0600

    Display Blog data and hashtags

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar></Navbar>
+      <div className="main-container flex text-center">
+        <div className="left-container w-[70%]">
+          <Blogs></Blogs>
+        </div>
+        <div className="right-container w-[30%]">
+          <h1>Reading time :0</h1>
+          <h1>bookmarked count :0</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
